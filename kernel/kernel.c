b8aa7622e1528ba72e90fe3afbefc865a1ccecc4
montaquila <akiel@akiel.org>
2022-09-05 20:35:29 -0700

    need to figure out solution for not UART-based protocols. How can I, down the road of course, implement using the GPIO pins for something SPI, I2C, etc. based. For now kernel.c is stubbed out according to some different resources. Verification will come when uart.c, uart.h and some others are completed as well. Will go back through and edit some docs/comments in the code to show better understanding

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,6 +1,7 @@
 /*
- * kernel.c containts the very root of the kernel. This file is where
- * we transfer over control from assembly in our boot_aarch*.S file
+ * kernel.c containts the very root of the kernelf for this OS. I hope for this 
+ * code to be easily portable to other architechtures as well. This file is where
+ * we transfer over control from assembly in our boot_aarch*.S file to C.
  *
  */
 
@@ -21,10 +22,15 @@ void kernel_main(uint32_t r0, uint32_t r1, uint32_t atags) {
     // initialize 
     uart_init();
     // print statement to console on successful execution
-    uart_puts("");
+    uart_puts("SUCCESS STMT:    1\r\n");
 
-
-
+    // infinite loop 
+    while (1) {
+        //
+        uart_putc(uart_getc());
+        //
+        uart_putc('\n');
+    }
 }
 
 
