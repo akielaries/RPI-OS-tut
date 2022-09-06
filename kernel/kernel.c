18eda6a2c9ea6ee085d8874f3835e2e617da5b7d
montaquila <akiel@akiel.org>
2022-09-05 20:20:34 -0700

    adding implemenation of kernel.c. Trying to get kernel up and running to display a msg to stdout. Will need a USB->TTL cable for verification of UART capabilities but will continue to implement this using some tutorials and test this properly down the road.

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,7 +1,30 @@
+/*
+ * kernel.c containts the very root of the kernel. This file is where
+ * we transfer over control from assembly in our boot_aarch*.S file
+ *
+ */
+
+// define different macros we may need
+#include<stddef.h>
+// more macros; overall easier for portability 
+#include<stdint.h>
+// import what is necessary for UART functionality
+#include<../include/core/uart.h>
+// more imports as we implement more "features"
+
+void kernel_main(uint32_t r0, uint32_t r1, uint32_t atags) {
+    // declare these registers as unused for now
+    (void) r0;
+    (void) r1;
+    (void) atags;
+
+    // initialize 
+    uart_init();
+    // print statement to console on successful execution
+    uart_puts("");
 
 
-void main() {
-    while (1);
+
 }
 
 
